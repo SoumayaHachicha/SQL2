6bd0457d9fb1589b33773a7ae180a183f63674de
soumaya <122887842+SoumayaHachicha@users.noreply.github.com>
2023-02-09 13:00:16 +0100

    Update CREATION2.sql

--- CREATION2.sql
+++ CREATION2.sql
@@ -4,3 +4,11 @@ url VARCHAR(255) NOT NULL,
 name VARCHAR(255) NOT NULL,
 description VARCHAR (255)
 )
+
+
+CREATE TABLE account_role(
+user_id INT NOT NULL,
+role_id INT NOT NULL,
+grant_date TIMESTAMP,
+PRIMARY KEY (user_id, role_id)
+);
